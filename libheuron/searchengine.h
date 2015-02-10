320f007ef5bdcb196a027b2bec1cb41ce5211981
Vurmux <vurmux@gmail.com>
2015-02-10 18:59:38 +0300

    Started to write search engine subroutines

---
+++ libheuron/searchengine.h
@@ -0,0 +1,21 @@
+/*
+  -------------------------------------------------------------------------
+  Name: searchengine.h
+  Author: vurmux@gmail.com
+  -------------------------------------------------------------------------
+  This module implements various search engines.
+  -------------------------------------------------------------------------
+*/
+
+namespace heuron {
+    
+    class SearchEngine {
+	/*
+	  Abstract class for generic search engines
+	 */
+    private:
+    public:
+	SearchEngine() = 0;
+    };
+
+}
