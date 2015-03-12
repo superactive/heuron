d2fa7389c33d19a2949dcdf431c32ced474d2a74
Vurmux <vurmux@gmail.com>
2015-03-12 21:08:55 +0300

    Added Wildcard parameter to search functions

--- libheuron/searchengine.cpp
+++ libheuron/searchengine.cpp
@@ -11,13 +11,13 @@ namespace heuron {
 
     MAX_LEN = 65536;
     
-    int SimpleSearchEngine::search_in_file(std::ifstream is) {
+    int SimpleSearchEngine::search_in_file(std::ifstream is, Wildcard wildcard) {
 	file_string = std::string("");
 	is.readsome(file_string, MAX_LEN);
 	return search_in_string(file_string);
     }
 
-    int SimpleSearchEngine::search_in_string(string target_string) {
+    int SimpleSearchEngine::search_in_string(string target_string, Wildcard wildcard) {
 
     }
     
